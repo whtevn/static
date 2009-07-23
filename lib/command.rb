e036c088a537fea4331fc9f7f1e2440e54b5583c
Evan Short <evan.short@gmail.com>
2009-07-23 12:02:37 -0500

    adding Command to the library

---
+++ lib/command.rb
@@ -0,0 +1,31 @@
+module MetaMark
+  class Command
+    attr_accessor :command, :name, :type, :definition, :args
+
+    def self.delimit(definition)
+      command    = definition.split("(")
+      definition = {:command => command[0].metamark_clean, :definition => definition}
+      command    = command[1].split(",").collect {|a| a = a.metamark_clean }
+      Command.new({:name => command[0],
+                   :type =>command[1],
+                   :args =>command[2]}.merge(definition))
+    end
+
+    def initialize(args={})
+      @args       = args[:args]
+      @name       = args[:name]
+      @type       = args[:type]
+      @command    = args[:command]
+      @definition = args[:definition]
+    end
+
+    def arg_string
+      "#{self.name}, :#{self.type}#{", :#{self.args}" if self.args}"
+    end
+
+    def print(as_end=false)
+      "<!--%% #{self.command}(#{self.arg_string}) #{"%%" if as_end}-->" 
+    end
+  end
+end
+
